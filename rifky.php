a2eb45786b27688a532859036c61150f0e1ea709
Rifkysien1 <93208051+Rifkysien1@users.noreply.github.com>
2021-12-15 16:39:35 +0100

    Create rifky.php

---
+++ rifky.php
@@ -0,0 +1,7 @@
+# simple-api
+<?php
+ini_set("auto_detect_line_endings", true);
+$data = file_get_contents('http://127.0.0.1/api/data.php');
+$decodedData = json_decode($data,true);
+var_dump($decodedData);
+?>
